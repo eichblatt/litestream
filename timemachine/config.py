6c6fad05a012b2390640762aba3b4a40504a896d
eichblatt <steve.eichblatt@gmail.com>
2023-04-08 18:36:28 -0400

    changed

--- timemachine/config.py
+++ timemachine/config.py
@@ -1,34 +1,17 @@
-import datetime
 import json
 import os
-import logging
-import subprocess
 import time
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-logger = logging.getLogger(__name__)
 try:
     from timemachine import controls
 except Exception as e:
-    logger.warning(f"Failied to import controls")
+    print(f"Failied to import controls")
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-OPTIONS_PATH = os.path.join(os.getenv("HOME"), ".timemachine_options.txt")
-DB_PATH = os.path.join(ROOT_DIR, "metadata")
+ROOT_DIR = None
+OPTIONS_PATH = None
+DB_PATH = None
 
 optd = {}
-
-
-def get_board_version():
-    try:
-        cmd = "board_version.sh"
-        raw = subprocess.check_output(cmd, shell=True)
-        raw = raw.decode()
-        if raw == "version 2\n":
-            return 2
-    except Exception:
-        return 1
 
 
 # State variables
@@ -69,10 +52,6 @@ select_pin = 4  # pin 4 ok w/ Sound card
 play_pause_pin = 20  # pin 18 interferes with sound card
 stop_pin = 2  # from the I2C bus (may need to connect to ground)
 ffwd_pin = 26  # pin 26 ok with sound card.
-if get_board_version() == 2:
-    rewind_pin = 21
-else:
-    rewind_pin = 3
 
 
 def default_options():
@@ -88,7 +67,7 @@ def default_options():
 
 
 def save_options(optd_to_save):
-    logger.debug(f"in save_options. optd {optd_to_save}")
+    print(f"in save_options. optd {optd_to_save}")
     options = {}
     f = open(OPTIONS_PATH, "r")
     tmpd = json.loads(f.read())
@@ -113,7 +92,7 @@ def load_options():
         f = open(OPTIONS_PATH, "r")
         tmpd = json.loads(f.read())
         for k in optd.keys():
-            logger.debug(f"Loading options key is {k}")
+            print(f"Loading options key is {k}")
             try:
                 if k in [
                     "AUTO_UPDATE_ARCHIVE",
@@ -121,18 +100,18 @@ def load_options():
                     "UPDATE_ARCHIVE_ON_STARTUP",
                 ]:  # make booleans.
                     tmpd[k] = tmpd[k].lower() == "true"
-                    logger.debug(f"Booleans k is {k}")
+                    print(f"Booleans k is {k}")
                 if k in ["COLLECTIONS", "FAVORED_TAPER"]:  # make lists from comma-separated strings.
-                    logger.debug(f"lists k is {k}")
+                    print(f"lists k is {k}")
                     c = [x.strip() for x in tmpd[k].split(",") if x != ""]
                     if k == "COLLECTIONS":
                         c = ["Phish" if x.lower() == "phish" else x for x in c]
                     tmpd[k] = c
             except Exception:
-                logger.warning(f"Failed to set option {k}. Using {optd[k]}")
+                print(f"Failed to set option {k}. Using {optd[k]}")
     except Exception:
-        logger.warning(f"Failed to read options from {OPTIONS_PATH}. Using defaults")
+        print(f"Failed to read options from {OPTIONS_PATH}. Using defaults")
     optd.update(tmpd)  # update defaults with those read from the file.
-    logger.info(f"in load_options, optd {optd}")
+    print(f"in load_options, optd {optd}")
     os.environ["TZ"] = optd["TIMEZONE"]
     time.tzset()
