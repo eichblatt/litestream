fd5be9d28f50cb7082f53ce9dd9e41067015c34f
eichblatt <steve.eichblatt@gmail.com>
2023-04-23 12:31:56 -0400

    reorganizing, still works

--- timemachine/config.py
+++ timemachine/config.py
@@ -2,14 +2,6 @@ import json
 import os
 import time
 
-import st7789
-import vga1_16x32 as font
-import fonts.vga1_bold_16x16 as bfont
-import fonts.vga1_16x16 as sfont
-from machine import SPI, Pin
-from rotary_irq_esp import RotaryIRQ
-
-
 ROOT_DIR = None
 OPTIONS_PATH = None
 DB_PATH = None
@@ -41,75 +33,6 @@ ON_TOUR = False
 EXPERIENCE = False
 TOUR_YEAR = None
 TOUR_STATE = 0
-
-# Hardware pins
-
-
-# Init screen
-def configure(rotation=0, buffer_size=0, options=0):
-    return st7789.ST7789(
-        SPI(1, baudrate=40000000, sck=Pin(12), mosi=Pin(11)),
-        128,
-        160,
-        reset=Pin(4, Pin.OUT),
-        cs=Pin(10, Pin.OUT),
-        dc=Pin(6, Pin.OUT),
-        backlight=Pin(5, Pin.OUT),
-        color_order=st7789.RGB,
-        inversion=False,
-        rotation=rotation,
-        options=options,
-        buffer_size=buffer_size,
-    )
-
-
-PlayPausePoly = [(0, 0), (0, 15), (15, 8), (0, 0)]
-RewPoly = [(7, 0), (0, 8), (7, 15), (7, 0), (15, 0), (8, 8), (15, 15), (15, 0)]
-FFPoly = [(0, 0), (0, 15), (8, 8), (0, 0), (8, 0), (8, 15), (15, 8), (8, 0)]
-
-# Configure display driver
-tft = configure(1, buffer_size=64 * 64 * 2)
-
-# Init and clear screen
-tft.init()
-tft.fill(st7789.BLACK)
-
-
-# Set up pins
-pPower = Pin(21, Pin.IN, Pin.PULL_UP)
-pSelect = Pin(47, Pin.IN, Pin.PULL_UP)
-pPlayPause = Pin(2, Pin.IN, Pin.PULL_UP)
-pStop = Pin(15, Pin.IN, Pin.PULL_UP)
-pRewind = Pin(16, Pin.IN, Pin.PULL_UP)
-pFFwd = Pin(1, Pin.IN, Pin.PULL_UP)
-pYSw = Pin(41, Pin.IN, Pin.PULL_UP)
-pMSw = Pin(38, Pin.IN, Pin.PULL_UP)
-pDSw = Pin(9, Pin.IN, Pin.PULL_UP)
-
-pLED = Pin(48, Pin.OUT)
-
-PowerLED = False
-
-# Initialise the three rotaries. First value is CL, second is DT
-# Year
-y = RotaryIRQ(
-    42,
-    40,
-    min_val=1966,
-    max_val=1995,
-    reverse=False,
-    range_mode=RotaryIRQ.RANGE_BOUNDED,
-    pull_up=True,
-    half_step=False,
-)
-# Month
-m = RotaryIRQ(
-    39, 18, min_val=1, max_val=12, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED, pull_up=True, half_step=False
-)
-# Day
-d = RotaryIRQ(
-    8, 7, min_val=1, max_val=31, reverse=False, range_mode=RotaryIRQ.RANGE_BOUNDED, pull_up=True, half_step=False
-)
 
 
 def default_options():
