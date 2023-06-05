0ec1b1a0e4d87d985e8cd85431662b29642aa061
Mike Alexander <ma261065@users.noreply.github.com>
2023-06-05 17:39:46 +1000

    Update BasicOggPlayer.py
    Change to have a separate buffer for the header

--- BasicOggPlayer.py
+++ BasicOggPlayer.py
@@ -6,8 +6,6 @@ ws_pin = Pin(14)    # Word clock output
 sd_pin = Pin(17)    # Serial data output
 
 block = False
-InBufferSize = 8192
-DecodedBufferSize = 132095 # Max size the I2S device will use for its buffer is 132095
 
 def get_redirect_location(headers):
     for line in headers.split(b"\r\n"):
@@ -27,33 +25,47 @@ def i2s_callback(t):
     global block
     print('*')
     block = False;
-    
+  
+# There is a bit of a balancing act going on with buffers here:
+# The Header buffer needs to be big enough to hold the header. By observation, headers are around 4k in size but I don't know how big they can get. So it is made a generous size (8k)
+# What is left in the Headerbuffer after the header has been processed is then copied to the Inbuffer. So the Inbuffer needs to be big enough to hold that. 
+# The Inbuffer is deliberately smaller than the Headerbuffer, because the decoder expands the data in the Inbuffer into data in the Outbuffer, and the Outbuffer can't be bigger than 129kb, as that is the max size of the I2S buffer.
+# So if the Inbuffer is too big, we will overflow the Outbuffer.
+# However, if the Inbuffer is too small then data that is left in the Headerbuffer after the header has been processed will overflow the Inbuffer
+
 def Play_URL(url, port, callback):
     global block    # Flag which we use to block operation until the I2S decoder has finished playing
+    HeaderBufferSize = 8192
+    InBufferSize = 4096
+    OutBufferSize = 132095 # Max size the I2S device will use for its buffer is 132095
 
-    StreamBuffer = bytearray(InBufferSize)  # An array to hold samples which we read from the network
-    StreamBufferMV = memoryview(StreamBuffer)
+    HeaderBuffer = bytearray(HeaderBufferSize)  # An array to hold the Header data of the stream
+    HeaderBufferMV = memoryview(HeaderBuffer)
+    
+    InBuffer = bytearray(InBufferSize)  # An array to hold samples which we read from the network
+    InBufferMV = memoryview(InBuffer)
 
-    DecodedDataBuffer = bytearray(DecodedBufferSize) # An array to hold decoded audio samples. This needs to be bigger than the incoming samples array as the decoder expands the data
-    DecodedDataBufferMV = memoryview(DecodedDataBuffer)
+    OutBuffer = bytearray(OutBufferSize) # An array to hold decoded audio samples. This needs to be bigger than the incoming samples buffer as the decoder expands the data
+    OutBufferMV = memoryview(OutBuffer)
         
     _, _, host, path = url.split('/', 3)
     addr = socket.getaddrinfo(host, port)[0][-1]
-    s = socket.socket()
+    sock = socket.socket()
 
     # Experimental. May not work
-    #s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 8192)
-    #s.settimeout(0.05)
+    #sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 8192)
+    #sock.settimeout(0.05)
 
     print("Getting", path, "from", host, "Port:", port)
-    s.connect(addr)
-    s.setblocking(False) # Tell the socket to return straight away
-    s.send(bytes('GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    sock.connect(addr)
+    sock.setblocking(False) # Tell the socket to return straight away (async)
+    
+    sock.send(bytes('GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
     
     # Read the response headers
     response_headers = b""
     while True:
-        header = s.readline()
+        header = sock.readline()
         #print(header)
         if header != None:
             response_headers += header.decode('utf-8')
@@ -66,38 +78,80 @@ def Play_URL(url, port, callback):
         if redirect_location:
             # Extract the new host, port, and path from the redirect location
             new_host, new_port, new_path = parse_redirect_location(redirect_location)
-            s.close()
+            sock.close()
             # Establish a new socket connection to the server
             print("Redirecting to", new_host, new_port, new_path)
-            s = socket.socket()
+            sock = socket.socket()
             addr = socket.getaddrinfo(new_host, new_port)[0][-1]
-            s.connect(addr)
-            s.setblocking(False) # Return straight away
-            s.send(bytes('GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n' % (new_path, new_host), 'utf8'))
+            sock.connect(addr)
+            sock.setblocking(False) # Return straight away
+            
+            sock.send(bytes('GET /%s HTTP/1.1\r\nHost: %s\r\n\r\n' % (new_path, new_host), 'utf8'))
 
             # Skip the response headers
             while True:
-                header = s.readline()
+                header = sock.readline()
                 if header == b"\r\n":
                     break
 
-    InHeader = True
     SamplesOut = 0
     TotalData = 0
+    data = -1
+    TimeStart = time.ticks_ms()
     
+    # Fill the header buffer. May take multiple reads
+    while (TotalData < HeaderBufferSize) and (data != 0):
+        data = sock.readinto(HeaderBufferMV[TotalData:], HeaderBufferSize - TotalData)
+        time.sleep_ms(2) # Let the WiFi thread have some CPU time (not sure if needed as it *may* run on a different thread)
+        #print('r:', data, time.ticks_ms(), end=' ')
+        if data != None:
+            TotalData += data
+
+    print ("Filled header buffer. Time:", time.ticks_ms() - TimeStart, "ms. Total Data:", TotalData)
+    
+    # Process the header when we first start streaming the data, to initialise the decoder & set up buffers
+    PlayerBuffer = bytearray(200000)     # Create a big buffer for the decoder to use (200kB seems enough)
+    PlayerBufferMV = memoryview(PlayerBuffer)
+
+    # Decode the header. This will return how many bytes it has consumed from the buffer
+    Used = Player.Vorbis_Start(PlayerBufferMV, HeaderBufferMV)
+    print("Header used", Used, "bytes")
+
+    # Copy what's left in the header buffer to the Inbuffer for later decoding
+    if Used < HeaderBufferSize:
+        InBufferMV[:(HeaderBufferSize - Used)] = HeaderBufferMV[-(HeaderBufferSize - Used):]
+        TotalData = HeaderBufferSize - Used
+    
+    # Get info about this stream
+    channels, sample_rate, setup_memory_required, temp_memory_required, max_frame_size = Player.Vorbis_GetInfo()
+    #print("MaxFrameSize", max_frame_size)
+
+    # Set up the first I2S peripheral (0), make it async with a callback
+    audio_out = I2S(0, sck=sck_pin, ws=ws_pin, sd=sd_pin, mode=I2S.TX, bits=16, format=I2S.STEREO if channels == 2 else I2S.MONO, rate=sample_rate, ibuf=OutBufferSize) # Max bufffer is 132095 (129kB). We have to assume 16-bit samples as there is no way to read it from the stream info.
+    audio_out.irq(i2s_callback) 
+
+    # Now decode the rest of the stream
     while True:
         data = -1
         TimeStart = time.ticks_ms()
         
-        # Fill the buffer. May take multiple reads
+        if TotalData == InBufferSize:    # If we get here with an already full buffer then something went wrong with the decoder
+            print("Something went wrong")
+            audio_out.deinit()
+            s.close()
+            Player.Vorbis_Close()
+            return
+        
+        # Fill the Inbuffer. May take multiple reads. We may exit with data == 0 which means we are at the end of the stream, but we still need to play the last buffer
         while (TotalData < InBufferSize) and (data != 0):
-            data = s.readinto(StreamBufferMV[TotalData:], InBufferSize - TotalData)
+            data = sock.readinto(InBufferMV[TotalData:], InBufferSize - TotalData)
             time.sleep_ms(2) # Let the WiFi thread have some CPU time (not sure if needed as it *may* run on a different thread)
             #print('r:', data, time.ticks_ms(), end=' ')
             if data != None:
-                TotalData = data + TotalData
+                TotalData += data
         
         print ("Filled buffer. Time:", time.ticks_ms() - TimeStart, "ms. Total Data:", TotalData)
+        TotalData = 0
         
         # We will block here (except for the first time through) until the I2S callback is fired. i.e. I2S has finished playing and needs more data
         while (block == True): 
@@ -105,63 +159,38 @@ def Play_URL(url, port, callback):
             continue
             
         print()
-                
-        if data == 0:
-            print("Stream finished")
-            audio_out.deinit()
-            s.close()
-            Player.Vorbis_Close()
-            break
-            
-        else: # We have some data
-            #print('w', time.ticks_ms(), end='') #str(data, 'utf8'), end='')
-            # Process the header when we first start streaming the data, to initialise the decoder & set up buffers
-            if InHeader:
-                InHeader = False
-                
-                # Create the Vorbis Player, and pass it a big buffer for it to use (200kB seems enough)
-                PlayerBuffer = bytearray(200000)
-                PlayerBufferMV = memoryview(PlayerBuffer)
-                                
-                # Decode the header. This will return how many bytes it has consumed from the buffer
-                Used = Player.Vorbis_Start(PlayerBufferMV, StreamBufferMV)
-                print("Header used", Used, "bytes")
-                
-                # Get info about this stream
-                channels, sample_rate, setup_memory_required, temp_memory_required, max_frame_size = Player.Vorbis_GetInfo()
-                #print("MaxFrameSize", max_frame_size)
-                
-                # Set up the first I2S peripheral (0), make it async with a callback
-                audio_out = I2S(0, sck=sck_pin, ws=ws_pin, sd=sd_pin, mode=I2S.TX, bits=16, format=I2S.STEREO if channels == 2 else I2S.MONO, rate=sample_rate, ibuf=DecodedBufferSize) # Max bufffer is 132095 (129kB)
-                audio_out.irq(i2s_callback) 
-           
-            # Loop around, consuming all the data from the stream buffer one chunk at a time and building up a decoded samples buffer, and finally send the decoded data to the DAC
-            else:
-                while True:
-                    #print("Calling decoder with offset", Used)
-                    BytesUsed, AudioSamples = Player.Vorbis_Decode(StreamBufferMV[Used:], InBufferSize - Used, DecodedDataBufferMV[(SamplesOut * channels * 2):])  # Multiply by 2 because we are assuming 16-bit samples
-                    #print("Result:", BytesUsed, AudioSamples)
-                    # BytesUsed is the number of bytes used from the buffer 
-                    # AudioSamples is the number of decoded audio samples available. Each sample is 2 bytes (16 bits) x number of channels, so usually will be 4 bytes
+                 
+        # We have some data. Repeatedly call the decoder to decode one chunk at a time from Inbuffer, and build up audio samples in Outbuffer
+        Used = 0
+        #print('w', time.ticks_ms(), end='') #str(data, 'utf8'), end='')
 
-                    if BytesUsed == 0:   # No more usable data in the buffer. There may still be some data at the end which is not enough to decode
-                        break
+        while True:
+            #print("Calling decoder with offset", Used)
+            BytesUsed, AudioSamples = Player.Vorbis_Decode(InBufferMV[Used:], InBufferSize - Used, OutBufferMV[(SamplesOut * channels * 2):])  # Multiply by 2 because we are assuming 16-bit samples
+            print("Result:", BytesUsed, AudioSamples)
+            # BytesUsed is the number of bytes used from the buffer 
+            # AudioSamples is the number of decoded audio samples available. Each sample is 2 bytes (16 bits) x number of channels, so usually will be 4 bytes
 
-                    SamplesOut += AudioSamples;
-                    Used += BytesUsed
+            if BytesUsed == 0:      # No more usable data in the Inbuffer. There may still be some data at the end which is not enough to decode
+                if data == 0:       # If the decoder has finished decoding the Inbuffer AND the last read was zero, then we must be at the end of the stream. Otherwise just break out of the loop, play any samples and fill the Inbuffer again
+                    print("Stream finished")
+                    audio_out.deinit()
+                    sock.close()
+                    Player.Vorbis_Close()
+                    return
+                break
 
-            # If we got audio data, play it
-            if SamplesOut > 0:
-                numout = audio_out.write(DecodedDataBufferMV[:(SamplesOut * channels * 2)]) # Returns straight away. Multiply by 2 because we are assuming 16-bit samples
-                SamplesOut = 0
-                block = True;
-                    
-            # We may still have some data at the end of the buffer which was too short to decode. If so, move it to the beginning
-            print(InBufferSize - Used, "bytes left")
-            if Used < InBufferSize:
-                StreamBufferMV[:(InBufferSize - Used)] = StreamBufferMV[-(InBufferSize - Used):]
-                TotalData = InBufferSize - Used
-            else:
-                TotalData = 0
-                
-            Used = 0
+            SamplesOut += AudioSamples;
+            Used += BytesUsed
+
+        # If we got audio data, play it
+        if SamplesOut > 0:
+            numout = audio_out.write(OutBufferMV[:(SamplesOut * channels * 2)]) # Returns straight away. Multiply by 2 because we are assuming 16-bit samples
+            SamplesOut = 0
+            block = True;
+
+        # We may still have some data at the end of the buffer which was too short to decode. If so, move it to the beginning
+        print(InBufferSize - Used, "bytes left")
+        if Used < InBufferSize:
+            InBufferMV[:(InBufferSize - Used)] = InBufferMV[-(InBufferSize - Used):]
+            TotalData = InBufferSize - Used
