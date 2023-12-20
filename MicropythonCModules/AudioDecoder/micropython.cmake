ea69f19d39d2e0dceb54e8d4e13304c43e56a9ae
Mike Alexander <ma261065@users.noreply.github.com>
2023-12-20 11:23:04 +1100

    Update micropython.cmake

--- MicropythonCModules/AudioDecoder/micropython.cmake
+++ MicropythonCModules/AudioDecoder/micropython.cmake
@@ -1,16 +1,15 @@
 # Create an INTERFACE library for our C module.
-add_library(usermod_Decoder INTERFACE)
+add_library(usermod_AudioDecoder INTERFACE)
 
 # Add our source files to the lib
-target_sources(usermod_Decoder INTERFACE
+target_sources(usermod_AudioDecoder INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/vorbis_decoder.cpp
     ${CMAKE_CURRENT_LIST_DIR}/mp3_decoder.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/VorbisDecoder.c
-    ${CMAKE_CURRENT_LIST_DIR}/MP3Decoder.c
+    ${CMAKE_CURRENT_LIST_DIR}/Decoder.c
 )
 
 # Add the current directory as an include directory.
-target_include_directories(usermod_Decoder INTERFACE
+target_include_directories(usermod_AudioDecoder INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
@@ -22,4 +21,4 @@ set(CXXFLAGS_USERMOD "${CXXFLAGS_USERMOD} -O3")
 #set_source_files_properties(${CMAKE_CURRENT_LIST_DIR}/MP3Decoder.c PROPERTIES COMPILE_OPTIONS -O3)
 
 # Link our INTERFACE library to the usermod target.
-target_link_libraries(usermod INTERFACE usermod_Decoder)
+target_link_libraries(usermod INTERFACE usermod_AudioDecoder)
