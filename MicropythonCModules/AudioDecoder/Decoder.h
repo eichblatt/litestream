4b232a5d4f745a892f342f275d060d61dea2728a
Mike Alexander <ma261065@users.noreply.github.com>
2023-12-22 11:53:02 +1100

    Use tracked allocs

--- MicropythonCModules/AudioDecoder/Decoder.h
+++ MicropythonCModules/AudioDecoder/Decoder.h
@@ -8,40 +8,14 @@
 //#include "esp_timer.h"
 
 const int MajorVersion = 1;
-const int MinorVersion = 0;
+const int MinorVersion = 1;
 
 typedef struct _Vorbis_Decoder_obj_t {
     mp_obj_base_t base;
     //int16_t a;
     //int16_t b;
-
-    // Vorbis Decoder Globals. All of these have mallocs/callocs done against them in vorbis_decoder.cpp, so must be members here to avoid the MicroPython GC collecting them
-    uint8_t *s_lastSegmentTable;
-    uint16_t *s_vorbisSegmentTable;
-    char* s_vorbisChbuf;
-    struct _vorbis_info_mapping *s_map_param;
-    struct _vorbis_info_mode *s_mode_param;
-    struct _vorbis_info_floor **s_floor_param;
-    struct _vorbis_info_residue *s_residue_param;
-    struct _codebook *s_codebooks;
-    int8_t *s_floor_type;
-    struct _vorbis_dsp_state *s_dsp_state;
 } Vorbis_Decoder_obj_t;
 
 typedef struct _MP3_Decoder_obj_t {
     mp_obj_base_t base;
-
-    // MP3 Decoder Globals. All of these have mallocs/callocs done against them in mp3_decoder.cpp, so must be members here to avoid the MicroPython GC collecting them
-    struct MP3DecInfo *m_MP3DecInfo;
-    struct FrameHeader *m_FrameHeader;
-    struct SideInfo *m_SideInfo;
-    struct ScaleFactorJS *m_ScaleFactorJS;
-    struct HuffmanInfo *m_HuffmanInfo;
-    struct DequantInfo *m_DequantInfo;
-    struct IMDCTInfo *m_IMDCTInfo;
-    struct SubbandInfo *m_SubbandInfo;
-    struct MP3FrameInfo *m_MP3FrameInfo;
 } MP3_Decoder_obj_t;
-
-extern Vorbis_Decoder_obj_t *mpVorbis;
-extern MP3_Decoder_obj_t *mpMP3;
